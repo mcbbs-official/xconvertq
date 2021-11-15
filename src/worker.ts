54b1ef8fb768cc5fae6d325a318c48c5ac79b038
bangbang93 <bangbang93@163.com>
2021-11-15 11:30:20 +0800

    替换字体

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,7 @@
 import bbob from '@bbob/core'
 import {render} from '@bbob/html/es'
 import html5Preset from '@bbob/preset-html5/es'
+import {isEmpty} from 'lodash'
 import * as TurndownService from 'turndown'
 import Piscina = require('piscina')
 
@@ -40,6 +41,7 @@ export default function processMessage(message: string): IMessageData {
 
 export function convertMessage(message: string): string {
   try {
+    message = replaceFontFamily(message)
     const html = convert.process(message, {render}).html
     if (Piscina.workerData.mode === 'markdown') {
       return turndownService.turndown(html)
@@ -51,4 +53,27 @@ export function convertMessage(message: string): string {
     console.error(e, message)
     return message
   }
+}
+
+function replaceFontFamily(message: string): string {
+  const fonts = {
+    宋体: 'SimSun',
+    新宋体: 'NSimSun',
+    黑体: 'SimHei',
+    微软雅黑体: 'Microsoft YaHei',
+    微软雅黑: 'Microsoft YaHei',
+    仿宋_GB2312: 'FangSong_GB2312',
+    楷体_GB2312: 'KaiTi_GB2312',
+  }
+  return message.replace(/\[font=([\W\D\w\s]*?)\]([\W\D\w\s]*?)\[\/font\]/iug, (...matches) => {
+    matches[1] = matches[1].replaceAll('&quot;', '').replaceAll(' ', '')
+    if (isEmpty(matches[1])) {
+      return matches[2]
+    }
+    if (fonts[matches[1]]) {
+      return `[font=${fonts[matches[1]]}]${matches[2]}[/font]`
+    } else {
+      return `[font=${matches[1]}${matches[2]}[/font]`
+    }
+  })
 }
